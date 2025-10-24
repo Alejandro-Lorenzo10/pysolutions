ae734a8e97cbbb25d0a26353486f4139ae674ceb
Alejandro Lorenzo <alejandrolorenzo@MacBookAir.lan>
2025-10-24 10:54:14 -0700

    fix: robust err() and finalize Storage KV implementation

--- utils.py
+++ utils.py
@@ -3,20 +3,29 @@ from colorama import Fore, Style, init as color_init
 
 color_init(autoreset=True)
 
+# Command aliases used by the client
 ALIASES = {
     "ls": "list",
     "rm": "remove",
-    "add": "add",
     "del": "remove",
+    "add": "add",
     "login": "login",
     "register": "register",
     "help": "help",
     "exit": "exit",
 }
 
-def ok(msg):   print(Fore.GREEN + "✔ " + msg)
-def err(msg):  print(Fore.RED   + "✘ " + msg)
-def info(msg): print(Fore.YELLOW+ "• " + msg)
+def ok(msg: str):
+    print(Fore.GREEN + "✔ " + str(msg))
+
+def err(msg):
+    # Be robust if msg is None
+    if msg is None:
+        msg = "Unknown error"
+    print(Fore.RED + "✘ " + str(msg))
+
+def info(msg: str):
+    print(Fore.YELLOW + "• " + str(msg))
 
 def prompt(user: str | None) -> str:
     name = user or "guest"
